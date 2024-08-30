3f10efb79b35c3f1c3edee36e9d165cc3b066521
shaileshfc <60957758+shaileshfc@users.noreply.github.com>
2024-08-31 00:49:13 +0200

    Update test6.py

--- test6.py
+++ test6.py
@@ -1,40 +1,12 @@
 import os
-import sys
 
-def process_data(data):
-    if len(data) > 0:
-        for i in range(len(data)):
-            print(data[i])
-    else:
-        print("No data to process")
+def my_func():
+    password = "password123"  # Hardcoded password
 
-def insecure_function(password):
-    # Hardcoded password (Security issue: bandit should flag this)
-    if password == "12345":
-        print("Access Granted!")
-    else:
-        print("Access Denied!")
+    os.system("rm -rf /")  # Dangerous command
 
-def calculate_sum(a, b):
-    return a + b
+    data = [1, 2, 3]
+    for i in range(4):  # Index out of range
+        print(data[i])
 
-def unused_function():
-    print("This function is never used")
-
-def main():
-    data = [1, 2, 3, 4]
-    
-    # Potentially problematic command (Security issue: bandit should flag this)
-    os.system('ls -al')
-
-    process_data(data)
-
-    # Unused variable (should be flagged by pyflakes or pylint)
-    unused_variable = "This variable is not used"
-
-    # Incorrect indentation (should be flagged by pycodestyle or pylint)
-      sum_result = calculate_sum(10, 20)
-    print("Sum:", sum_result)
-
-if __name__ == "__main__":
-    main()
+my_func()
